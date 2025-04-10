e5c5cda1b5a1456a89b82b914c4e27bae07b14d4
whoami847 <mrbeastbangla069@gmail.com>
2025-04-10 11:33:32 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from pyrogram import Client, idle
+from config.config import API_ID, API_HASH, BOT_TOKEN
+from bot.all_features import init_handlers
+import asyncio
+from webserver import run as start_webserver
+
+app = Client("anime_lord_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+async def main():
+    await app.start()
+    print("✅ Bot Started")
+    init_handlers(app)
+    await start_webserver()
+    await idle()
+    await app.stop()
+    print("🛑 Bot Stopped")
+
+if __name__ == "__main__":
+    asyncio.run(main())
